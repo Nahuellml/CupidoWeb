280b65e7901610f41ea6d7a62c4df9002a79cbc4
Nahuel Alvarez <alvareznahue@gmail.com>
2024-08-15 21:18:08 -0300

    color mesh

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ import { GLTFLoader } from 'three/examples/jsm/Addons.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const loader = new GLTFLoader();
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -12,15 +13,19 @@ document.body.appendChild( renderer.domElement );
 
 const controls = new OrbitControls( camera, renderer.domElement );
 
-
-const loader = new GLTFLoader();
+/* const material = new THREE.MeshStandardMaterial({
+  color: 0xff0000, // Color base
+  emissive: 0x000000, // Luz propia del material
+  roughness: 0.5, // Rugosidad del material
+  metalness: 0.5 // Metalidad
+}); */
 
 loader.load( '/CupidoWeb/assets/prueba13D.glb', function ( gltf ) {
   const object = gltf.scene;
   object.traverse(function (child) {
-    if(child.isMesh) {
-      child.material.color.set(0xff0000);
-    }
+    /* if(child.isMesh) {
+      child.material.color.set(0xffff00);
+    } */
   })
 	scene.add( object );
 
