fa9c2560fc83f1f2b63b44f7f9787a1c9c61ca5a
Nahuel Alvarez <alvareznahue@gmail.com>
2024-08-17 23:11:06 -0300

    modal added

--- src/handlers/buttonHandler.js
+++ src/handlers/buttonHandler.js
@@ -2,6 +2,8 @@ import * as THREE from 'three';
 
 export function setupButton(deliberyMan, camera) {
     const btnOrderHandler = document.querySelector('.btn-order');
+    const modal = document.getElementById('modal');
+    const closeModalBtn = document.getElementById('closeModalBtn');
     let targetRotationY = 0;
     let cameraRotationY = 0;
 
@@ -10,7 +12,12 @@ export function setupButton(deliberyMan, camera) {
         targetRotationY = 10;
         cameraRotationY = 2;
         btnOrderHandler.style.animationPlayState = 'running';
+        modal.showModal()
     });
+
+    closeModalBtn.addEventListener('click', () => {
+        modal.close();
+    })
 
     return function animateModel() {
         deliberyMan.rotation.y = THREE.MathUtils.lerp(deliberyMan.rotation.y, targetRotationY, 0.035);
